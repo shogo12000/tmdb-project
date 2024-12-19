8150fe6a98b5266f2d1623fb375c3ce69a5137b5
sidney <shogo12000@hotmail.com>
2024-12-18 17:27:12 -0800

    create Routes and pages

---
+++ src/pages/Movies.jsx
@@ -0,0 +1,12 @@
+
+
+function Movies(){
+    return (
+ 
+        <h1>Movies</h1>
+   
+
+    )
+}
+
+export default Movies
---
+++ src/pages/People.jsx
@@ -0,0 +1,9 @@
+
+
+function People(){
+    return (
+        <h1>People</h1>
+    )
+}
+
+export default People
